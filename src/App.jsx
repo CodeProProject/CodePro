864659f03243709383e0e0a8802e3258a3cdb24d
klebersilvars <kleber.600.l@gmail.com>
2023-12-21 22:07:09 -0300

    menu navegacao atualizado
    menu navegacao atualizado

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-import Header from './Components/Header/Header';
+import Navegacao from './Components/Navegacao/Navegacao';
 
 const App = () => {
   return (
-    <Header/>
+    <Navegacao/>
   )
 }
 
