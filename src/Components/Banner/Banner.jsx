8c958e5f0a3c486697bdc509b4bcdc86d42e1b3f
Guimaraes <caique.guimaraesdeoliveira306@gmail.com>
2023-12-21 22:48:48 -0300

    banner finalizado

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 const Banner = () => {
   return (
-    <section className='w-full h-[600px] bg-red-500'>
-      
+    <section className='w-full h-[600px] bg-banner bg-right bg-cover bg-fixed bg-no-repeat'>
+      <div className='w-full h-full bg-black/30'>
+
+      </div>
     </section>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,jsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        banner: 'url(assets/CodeBanner1.jpg)',
+      },
+    },
   },
   plugins: [],
 }
