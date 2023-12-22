0bcd702d05182700eaa717ff59401e43eed345e2
Guimaraes <caique.guimaraesdeoliveira306@gmail.com>
2023-12-21 22:39:49 -0300

    componente main e banner

---
+++ src/Components/Banner/Banner.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Banner = () => {
+  return (
+    <section className='w-full h-[400px] bg-red-500'>
+      
+    </section>
+  )
+}
+
+export default Banner;
---
+++ src/Components/MainConteiner/MainConteiner.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import Banner from '../Banner/Banner';
+
+const MainConteiner = () => {
+  return (
+    <main className='w-full h-screen'>
+      <Banner />
+    </main>
+  )
+}
+
+export default MainConteiner;
