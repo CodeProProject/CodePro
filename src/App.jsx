3ef2bd8046ad2f8a4ab4fa403cf2fbdd2322922d
Guimaraes <caique.guimaraesdeoliveira306@gmail.com>
2023-12-23 22:46:08 -0300

    organização de hierarquias

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import Cabecalho from './Components/Cabecalho/Cabecalho';
 import MainConteiner from "./Components/MainConteiner/MainConteiner";
-import Section from './Components/Section/Section';
 
 function App() {
   return (
     <>
       <Cabecalho />
+      
       <MainConteiner />
-      <Section/>
     </>
   )
 }
--- src/Components/MainConteiner/MainConteiner.jsx
+++ src/Components/MainConteiner/MainConteiner.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 
 import Banner from '../Banner/Banner';
+import Section from '../Section/Section';
 
 const MainConteiner = () => {
   return (
     <main className='w-full h-full'>
       <Banner />
+
+      <Section/>
     </main>
   )
 }
