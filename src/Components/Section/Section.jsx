c5099afe528dd199d48ed40177fcdee555e704ec
klebersilvars <kleber.600.l@gmail.com>
2023-12-23 23:18:50 -0300

    alteração no menu sobre a codepro
    alteração no menu sobre a codepro
    
    finalizei este componente

--- src/Components/Section/Section.jsx
+++ src/Components/Section/Section.jsx
@@ -2,6 +2,17 @@ import React from 'react'
 import "./Section.css";
 import designOtimizar from '../../assets/designOtimizar.png'
 
+//texto para por no diferencial 
+/*O Que Nos Diferencia:
+
+Personalização Excepcional: Cada projeto é único, e personalizamos nossas soluções para atender às necessidades específicas de cada cliente. Seja um site elegante, um aplicativo funcional ou uma combinação dos dois, nossa equipe se dedica a entregar resultados excepcionais.
+
+Tecnologia de Ponta: Ficamos à frente das últimas tendências tecnológicas para garantir que suas soluções digitais não apenas atendam aos padrões atuais, mas também estejam prontas para o futuro. Utilizamos as mais recentes ferramentas e frameworks para criar produtos robustos e de alto desempenho.
+
+Experiência do Usuário Aprimorada: Entendemos a importância da experiência do usuário. Criamos interfaces intuitivas e atraentes que não apenas impressionam visualmente, mas também proporcionam uma jornada fácil e envolvente para seus usuários.
+
+Compromisso com a Excelência: A excelência é a nossa norma. Buscamos constantemente aperfeiçoar nossas habilidades, processos e produtos para garantir que ofereçamos serviços que superem as expectativas. */
+
 const Section = () => {
   return (
     <div id='sobre' className='section-container'>
@@ -10,8 +21,11 @@ const Section = () => {
         </div>
 
         <div className='info-texto-container'>
-            <h1>Sobre</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam architecto explicabo dolorum nisi quae ratione eius quis quia rerum corrupti magnam necessitatibus in voluptatem nemo, earum inventore voluptatibus, itaque quod.</p>
+            <h3>A CodePro é uma empresa especializada no desenvolvimento de sites e aplicativos sob medida.</h3>
+
+            <p>Nossa abordagem centrada no cliente e nossa paixão pela tecnologia nos destacam como uma escolha líder para aqueles que buscam uma presença digital marcante. Com uma equipe de especialistas criativos e engenheiros de software altamente qualificados, estamos comprometidos em oferecer produtos digitais excepcionais que impulsionam o sucesso dos nossos clientes.</p>
+
+            
         </div>
         
     </div>
